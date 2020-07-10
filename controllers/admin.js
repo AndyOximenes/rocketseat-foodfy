8c68b86a27f7a59168fc436e304a7a71abc997ed
AndyOximenes <andy.oximenes@gmail.com>
2020-07-10 16:42:01 -0300

    Add admin index page

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 const data = require("../data");
 
 exports.index = (request, response) => {
-  return response.render("admin/index");
+  return response.render("admin/index", { items: data });
 };
